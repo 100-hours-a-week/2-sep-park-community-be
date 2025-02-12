677030a96282f8c5f04e28fd08a696a6856b7a59
sep.park <dytpq0978@naver.com>
2025-02-13 01:40:02 +0900

    samesite

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(
             httpOnly: true,
             secure: false,
             maxAge: 60 * 60 * 1000,
-            sameSite: 'None'
+            sameSite: 'None'/
         },
     })
 );
